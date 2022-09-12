fbcd5a9dda9fd594fa76882c3c801512f734f7dc
_Nathan Nguyen_(student) <48932@cv.k12.ca.us>
2022-09-12 16:52:04 -0700

    Initial commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        String currLine = "#";
+        int i = 0;
+        while (i<5){
+            System.out.println(currLine);
+            currLine = currLine + "#"; 
+            i++;
+        } 
+    }
+}     
+
+
